d9b43d6453b51023b8e0b8dd65aec50ebe0991ca
Kanstantsin Kulak <kkulak24@gmail.com>
2021-05-24 16:49:44 +0200

    feat: complete DateHelper tasks

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -4,26 +4,49 @@
 
 #pragma mark - First
 
--(NSString *)monthNameBy:(NSUInteger)monthNumber {
+- (NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if (0 < monthNumber && monthNumber <= 12) {
+        NSDateFormatter *df = [NSDateFormatter new];
+        NSString *monthName = [df monthSymbols][monthNumber - 1];
+        return monthName;
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *df = [NSDateFormatter new];
+    [df setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    NSDate *parsedDate = [df dateFromString:date];
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:parsedDate];
+    return components.day;
 }
 
 #pragma mark - Third
 
-- (NSString *)getDayName:(NSDate*) date {
+- (NSString *)getDayName:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateFormatter *df = [NSDateFormatter new];
+    [df setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"]];
+
+    NSInteger dayN = [calendar component:NSCalendarUnitWeekday fromDate:date];
+
+    if (dayN) {
+        return [df shortWeekdaySymbols][(NSUInteger) (dayN - 1)];
+    }
+
     return nil;
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger dateWeek = [calendar component:NSCalendarUnitWeekOfYear fromDate:date];
+    NSInteger currentWeek = [calendar component:NSCalendarUnitWeekOfYear fromDate:[NSDate now]];
+
+    return dateWeek == currentWeek;
 }
 
 @end
